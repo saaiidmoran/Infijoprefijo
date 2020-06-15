9ec40df8cb906d6e02b73ee9fd374b1a6e0dfb6c
Said Isaac Jorge Moran <saaiidmoran@gmail.com>
2020-06-14 21:11:31 -0500

    Modifiqué el archivo conversionExpresion.java

--- src/com/saaiidmoran/infijoprefijo/service/conversionExpresion.java
+++ src/com/saaiidmoran/infijoprefijo/service/conversionExpresion.java
@@ -12,24 +12,27 @@ package com.saaiidmoran.infijoprefijo.service;
 import com.saaiidmoran.infijoprefijo.pila.Pila;
  
 public class conversionExpresion {
- 
     
-   public String Infijo2PrefijoTxt(String infijo){
-        Pila p1 = Infijo2Prefijo(infijo);
+    private final String simbols = "+-*/()^="; 
+    
+    public String Infijo2PrefijoTxt(String infijo){
+         Pila p1 = Infijo2Prefijo(infijo);
+         String text = "";
+         while (p1.i > 0){
+             text += p1.pop();
+         }
+          return text;
+
+     }
+    public String Infijo2PosfijoTxt(String infijo){
+        Pila p1 = Infijo2Posfijo(infijo);
         String text = "";
-        while (p1.i > 0)
-            text += p1.pop();
-        return text;
- 
-    }
-   public String Infijo2PosfijoTxt(String infijo){
-    Pila p1 = Infijo2Posfijo(infijo);
-        String text = "";
-        while (p1.i > 0)
-        text = p1.pop() + text;
+        while (p1.i > 0){
+            text = p1.pop() + text;
+        }         
         return text;
     }
-    public static Pila Infijo2Prefijo(String infijo) {
+    private Pila Infijo2Prefijo(String infijo) {
         infijo = '(' + infijo ; // Agregamos al final del infijo un ')'
         int tamaño = infijo.length();
         Pila PilaDefinitiva = new Pila(tamaño);
@@ -42,7 +45,7 @@ public class conversionExpresion {
                 PilaTemp.push(caracter);
                 break;
             case '+':case '-':case '^':case '*':case '/': case '=':
-                while (Jerarquia(caracter) > Jerarquia(PilaTemp.nextPop()))
+                while (jerarquiaSignosPrefijo(caracter) > jerarquiaSignosPrefijo(PilaTemp.nextPop()))
                     PilaDefinitiva.push(PilaTemp.pop());
                 PilaTemp.push(caracter);
                 break;
@@ -58,79 +61,86 @@ public class conversionExpresion {
         return PilaDefinitiva;
     }
     
- public static Pila Infijo2Posfijo(String infijo) {
-infijo += ')'; // Agregamos al final del infijo un ‘)’
-int tamaño = infijo.length();
-Pila PilaDefinitiva = new Pila(tamaño);
-Pila PilaTemp = new Pila(tamaño);
-PilaTemp.push('('); // Agregamos a la pila temporal un ‘(‘
-for (int i = 0; i < tamaño; i++) { 
-    char caracter = infijo.charAt(i);
-    switch (caracter) {
-        case '(': PilaTemp.push(caracter); break; 
-        case '+':case '-':case '^':case '*':case '/': case '=': 
-            while (Jerarquia1(caracter) <= Jerarquia1(PilaTemp.nextPop()))
-                PilaDefinitiva.push(PilaTemp.pop());
-                 PilaTemp.push(caracter);
-            break;
-        case ')': 
-            while (PilaTemp.nextPop() != '(')
-                PilaDefinitiva.push(PilaTemp.pop());
-            PilaTemp.pop();
-            break;
-        default:
-            PilaDefinitiva.push(caracter);
-    } 
-} return PilaDefinitiva;
- }
- public static int Jerarquia1(char elemento) {
-     int res = 0; 
-     switch (elemento) {
-         case ')':  
-             res = 5;
-         break;
-         case '^':
-             res = 4;
-             break; 
-         case '*': case '/':
-             res = 3;
-             break;
-               case '+': case '-': case '=': 
-               res = 2;
-             break;
-               case '(': 
-                   res = 1; break;          
-     } return res; 
- }
+    private Pila Infijo2Posfijo(String infijo) {
+       infijo += ')'; // Agregamos al final del infijo un ‘)’
+       int tamaño = infijo.length();
+       Pila PilaDefinitiva = new Pila(tamaño);
+       Pila PilaTemp = new Pila(tamaño);
+       PilaTemp.push('('); // Agregamos a la pila temporal un ‘(‘
+       for (int i = 0; i < tamaño; i++) { 
+           char caracter = infijo.charAt(i);
+           switch (caracter) {
+               case '(': PilaTemp.push(caracter); break; 
+               case '+':case '-':case '^':case '*':case '/': case '=': 
+                   while (jerarquiaSignosPostfijo(caracter) <= jerarquiaSignosPostfijo(PilaTemp.nextPop()))
+                       PilaDefinitiva.push(PilaTemp.pop());
+                        PilaTemp.push(caracter);
+                   break;
+               case ')': 
+                   while (PilaTemp.nextPop() != '(')
+                       PilaDefinitiva.push(PilaTemp.pop());
+                   PilaTemp.pop();
+                   break;
+               default:
+                   PilaDefinitiva.push(caracter);
+           } 
+       } return PilaDefinitiva;
+    }
+    private int jerarquiaSignosPostfijo(char elemento) {
+        int res = 0; 
+        switch (elemento) {
+            case ')':  
+                res = 5;
+                break;
+            case '^':
+                res = 4;
+                break; 
+            case '*': case '/':
+                res = 3;
+                break;
+            case '+': case '-': case '=': 
+                res = 2;
+                break;
+            case '(': 
+                res = 1; 
+                break;          
+        } return res; 
+    }
 
-    public static int Jerarquia(char elemento) {
+    private int jerarquiaSignosPrefijo(char elemento) {
         int res = 0;
         switch (elemento) {
-        case ')': case '=':
-            res = 5; break;
-        case '^':
-            res = 2; break;
-        case '*': case '/':
-            res = 3; break;
-        case '+': case '-': 
-            res = 4; break;
-        case '(':
-            res = 1; break;
+            case ')': case '=':
+                res = 5; 
+                break;
+            case '^':
+                res = 2; 
+                break;
+            case '*': case '/':
+                res = 3; 
+                break;
+            case '+': case '-': 
+                res = 4; 
+                break;
+            case '(':
+                res = 1; 
+                break;
         }
         return res;
     }
     
     public String Depurar(String s) {
-    s = s.replaceAll("\\s+", ""); //Elimina espacios en blanco
-    String simbols = "+-*/()^=";
-    String str = "";
-  
-    //Deja espacios entre operadores
-    for (int i = 0; i < s.length(); i++) {
-      if (simbols.contains("" + s.charAt(i))) {
-        str += " " + s.charAt(i) + " ";
-      }else str += s.charAt(i);
+        String sSinEspacios = s.replaceAll("\\s+", ""); //Elimina espacios en blanco
+        //Deja espacios entre operadores
+        String str = ""; 
+        for (int i = 0; i < sSinEspacios.length(); i++) {        
+          if(simbols.contains("" + sSinEspacios.charAt(i))) {
+            str += " " + sSinEspacios.charAt(i) + " ";
+          }else{
+              str += sSinEspacios.charAt(i);
+          }      
+        }
+        return str.replaceAll("\\s+", " ").trim(); //Retorna la expresion depurada 
+        //.trim para quitarle los espacios al principio y al final solamente
     }
-    return str.replaceAll("\\s+", " ").trim();
-  }
 }
